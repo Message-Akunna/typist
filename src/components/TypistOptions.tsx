54b1a722b1d12d35b4eb8f70afe5b4b14346d34f
Message Akunna <message.ao@gmail.com>
2022-05-19 12:19:07 +0100

    designs done

--- src/components/TypistOptions.tsx
+++ src/components/TypistOptions.tsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 // next components
 import { useRouter } from 'next/router';
-// react bootstrap components 
+// react bootstrap components
 import ListGroup from 'react-bootstrap/ListGroup';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-// 
+//
 import { TypistOptionsContext } from '../context/TypistOptionsContext';
 
 const TypistOptions = () => {
@@ -14,24 +14,30 @@ const TypistOptions = () => {
   return (
     <Offcanvas show={showOptions} onHide={handleCloseOptions} placement='end' scroll={true} backdrop={false}>
       <Offcanvas.Header closeButton className='border-bottom py-2'>
-        <Offcanvas.Title className="py-1">Typist options</Offcanvas.Title>
+        <Offcanvas.Title className='py-1'>Typist options</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body className='px-0'>
-        <h6 className='px-3'>Easy - <span className='text-muted fw-normal'>Time: </span> 5:00 minutes</h6>
+        <h6 className='px-3'>
+          Easy - <span className='text-muted fw-normal'>Time: </span> 5:00 minutes
+        </h6>
         <ListGroup variant='flush' className='border-bottom border-top mb-3'>
           <ListGroup.Item action>Cras justo odio</ListGroup.Item>
           <ListGroup.Item action>Dapibus ac facilisis in</ListGroup.Item>
           <ListGroup.Item action>Morbi leo risus</ListGroup.Item>
           <ListGroup.Item action>Porta ac consectetur ac</ListGroup.Item>
         </ListGroup>
-        <h6 className='px-3'>Medium -  <span className='text-muted fw-normal'>Time: </span> 3:00 minutes</h6>
+        <h6 className='px-3'>
+          Medium - <span className='text-muted fw-normal'>Time: </span> 3:00 minutes
+        </h6>
         <ListGroup variant='flush' className='border-bottom border-top mb-3'>
           <ListGroup.Item action>Cras justo odio</ListGroup.Item>
           <ListGroup.Item action>Dapibus ac facilisis in</ListGroup.Item>
           <ListGroup.Item action>Morbi leo risus</ListGroup.Item>
           <ListGroup.Item action>Porta ac consectetur ac</ListGroup.Item>
         </ListGroup>
-        <h6 className='px-3'>Hard -  <span className='text-muted fw-normal'>Time: </span> 1:00 minute</h6>
+        <h6 className='px-3'>
+          Hard - <span className='text-muted fw-normal'>Time: </span> 1:00 minute
+        </h6>
         <ListGroup variant='flush' className='border-bottom border-top mb-3'>
           <ListGroup.Item action>Cras justo odio</ListGroup.Item>
           <ListGroup.Item action>Dapibus ac facilisis in</ListGroup.Item>
@@ -41,6 +47,6 @@ const TypistOptions = () => {
       </Offcanvas.Body>
     </Offcanvas>
   );
-}
+};
 
 export default TypistOptions;
