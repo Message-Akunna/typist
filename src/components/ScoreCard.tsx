25881da55f4fbac7cb55ff6a34df1da19872be46
Message Akunna <message.ao@gmail.com>
2022-05-24 13:23:44 +0100

    feedback corrections on words completed

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,35 +1,42 @@
 import React from 'react';
-// 
+//
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 
 interface Props {
+  correctWordIndexArray: Array<number>;
+  incorrectWordIndexArray: Array<number>;
   correct: number;
   incorrect: number;
 }
 
-const ScoreCard = ({
-  correct,
-  incorrect
-} : Props) => {
-
+const ScoreCard = ({ correctWordIndexArray, incorrectWordIndexArray, correct, incorrect }: Props) => {
   return (
     <section className=''>
-      <h3 className='text-center'>Checkout your score !!!</h3>
-      <div className='d-flex justify-content-center center align-items-center mb-3'>
-        <div className='' style={{ width: '160px' }}>
-          <CircularProgressbarWithChildren value={Math.round((correct / (correct + incorrect)) * 100)}>
-            <div className='text-center'>
-              <h2 className='mb-0'>
-                <strong>{correct !== 0? Math.round((correct / (correct + incorrect)) * 100) : 0}%</strong>
-              </h2>
-              <p className='text-muted mb-0'>Accuracy</p>
-            </div>
-          </CircularProgressbarWithChildren>
+      <h3 className='text-center mb-4'>Checkout your score !!!</h3>
+      <div className='d-flex flex-column flex-md-row justify-content-center align-items-md-center'>
+        <div className='d-flex justify-content-start align-items-center mb-3'>
+          <div className='' style={{ width: '160px' }}>
+            <CircularProgressbarWithChildren value={Math.round((correct / (correct + incorrect)) * 100)}>
+              <div className='text-center'>
+                <h2 className='mb-0'>
+                  <strong>{correct !== 0 ? Math.round((correct / (correct + incorrect)) * 100) : 0}%</strong>
+                </h2>
+                <p className='text-muted mb-0'>Accuracy</p>
+              </div>
+            </CircularProgressbarWithChildren>
+          </div>
         </div>
-      </div>
-      <div className='text-center mt-3'>
-        <h2 className='h1'>{correct}</h2>
-        <p className='text-muted'>Words per minute:</p>
+        <div className='mt-3 mt-md-0 ms-0 ms-md-5'>
+          <p className='text-muted fs-5 fw-normal'>
+            Words per minute : <span className='fw-bold text-dark'>{correct}</span>
+          </p>
+          <p className='text-muted fs-5 fw-normal'>
+            Correct word count : <span className='fw-bold text-success'>{correctWordIndexArray.length}</span>
+          </p>
+          <p className='text-muted fs-5 fw-normal'>
+            Incorrect word count : <span className='fw-bold text-danger'>{incorrectWordIndexArray.length}</span>
+          </p>
+        </div>
       </div>
     </section>
   );
